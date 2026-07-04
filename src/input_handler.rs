12b148f9a40f68f6d468b04d9e3326883558b36f
usep-arch <s93753564@gmail.com>
2026-07-04 16:02:29 +0600

    feat: Two input validator for game

---
+++ src/input_handler.rs
@@ -0,0 +1,65 @@
+// random_number_guessing/src/input_handler.rs
+
+use std::io::{self, Write};
+
+pub fn take_integer(prompt: &str) -> i32 {
+    let mut input = String::new();
+
+    loop {
+        print!("{}: ", prompt);
+        io::stdout().flush().expect("Failed to flush");
+
+        input.clear();
+
+        // Take input
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                if input.trim().is_empty() {
+                    println!("Input can't be empty! Please try again");
+                    continue;
+                }
+            }
+
+            Err(_) => {
+                println!("Something went wrong! Goodbye");
+                std::process::exit(0);
+            }
+        }
+
+        // Made String to integer(i32)
+        match input.trim().parse::<i32>() {
+            Ok(num) => return num,
+            Err(_) => {
+                println!("Invalid integer! Please enter a valid integer");
+                continue;
+            }
+        }
+    }
+}
+
+pub fn take_string(prompt: &str) -> String {
+    let mut input = String::new();
+
+    loop {
+        print!("{}: ", prompt);
+        io::stdout().flush().expect("Failed to flush");
+
+        input.clear();
+
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                if input.trim().is_empty() {
+                    println!("Input can't be empty! Please try again");
+                    continue;
+                } else {
+                    return input.trim().to_string();
+                }
+            }
+
+            Err(_) => {
+                println!("Something went wrong! Goodbye");
+                std::process::exit(0);
+            }
+        }
+    }
+}
